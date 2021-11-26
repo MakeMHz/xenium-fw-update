c27fd0d97eb7c7e831ee38233692f5513638a369
tuxuser <462620+tuxuser@users.noreply.github.com>
2021-11-26 05:54:16 +0100

    Update additional known versions
    Added hashes for
    * XOS 2.0.1 BLUE
    * XOS 2.0.2 BLUE
    * XOS 2.1.0 BLUE
    * XOS 2.2.1 BLUE
    * XOS 2.3.0 BLUE
    * XOS 2.3.1 GOLD

--- src/versions.cpp
+++ src/versions.cpp
@@ -1,5 +1,14 @@
 #include "versions.h"
 
-const Version XeniumVersions[1] = {
+const Version XeniumVersions[7] = {
+    // Gold
+    { "XOS Gold v2.3.1", 0x12B3B71A, 0x4C199DDC },
+    // Blue
+    // XOS Blue v2.0.0 missing
+    { "XOS Blue v2.0.1", 0x03E1DC69, 0xB67BE3C7 },
+    { "XOS Blue v2.0.2", 0xDCF9D35A, 0xE78934A3 },
+    { "XOS Blue v2.1.0", 0x7ACD0798, 0x4D331FA0 },
+    { "XOS Blue v2.2.1", 0x6D3BBFC2, 0xC29AA05D },
+    { "XOS Blue v2.3.0", 0x099637BA, 0x69DBB621 },
     { "XOS Blue v2.3.1", 0x0A3D160A, 0x1CF10F96 },
 };
