4231b7596fa12e093ea364db246e731a83846967
Dustin Holden <dustin@devholden.com>
2022-05-24 00:00:49 -0400

    Add v2.3.3 Patched to version list

--- src/versions.cpp
+++ src/versions.cpp
@@ -1,6 +1,7 @@
 #include "versions.h"
 
-const Version XeniumVersions[2] = {
+const Version XeniumVersions[3] = {
     { "XOS Blue v2.3.1",              0x0A3D160A, 0x1CF10F96 },
     { "XOS - MakeMHz v2.3.2 Patched", 0x0A3D160A, 0xB2479C24 },
+    { "XOS - MakeMHz v2.3.3 Patched", 0x0A3D160A, 0x865EE382 },
 };
